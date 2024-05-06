d59d1a714f9f8d48d3f3cf9fccfe6da368f12598
Sagnik Biswas <75833389+Sagnik-web@users.noreply.github.com>
2024-05-06 10:29:57 +0000

    date time

---
+++ 01_basic/06_Date_Time.js
@@ -0,0 +1,31 @@
+const date = new Date()
+
+const dt = Date()
+
+// console.log(typeof dt);
+// console.log(typeof date);
+
+// console.log(date.toString());
+
+// console.log(date.toLocaleDateString());
+
+// console.log(date.toDateString());
+
+// console.log(date.toISOString());
+// console.log(date.toJSON());
+
+// console.log(date.toUTCString());
+// console.log(date.toTimeString());
+
+// console.log(date.getTime());
+
+// console.log(date.getFullYear());
+// console.log(date.getMonth());
+
+// console.log(date.getMinutes());
+
+
+const newdt = new Date("2024-04-06")
+const inddt = new Date("05/06/2024")
+
+console.log(inddt.toDateString());
