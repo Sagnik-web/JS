66e66914322b94a32b9b30d762a1723aa565fc68
Sagnik Biswas <75833389+Sagnik-web@users.noreply.github.com>
2024-05-07 17:47:32 +0000

    object add

---
+++ 02_Core/02_Object.js
@@ -0,0 +1,58 @@
+const symbol = Symbol("123")
+
+const obj = {
+    name:"SB",
+    roll:4,
+    objFun:function () {
+        console.log("Object Inner Function");
+    },
+    [symbol]:"ABC"
+}
+
+// console.log(obj);
+
+// console.log(obj.name);
+// console.log(obj["name"]);
+
+const newObj = {
+    "name":"Sagnik Biswas",
+    "roll":6   
+
+} 
+// console.log(newObj.name);
+
+
+// obj.objFun();
+// console.log(typeof obj[symbol]);
+
+
+
+const allObj = {
+    1:"a",
+    2:"b",
+    3:{
+        4:"a",
+        5:"b",
+        6:{
+            7:"a",
+            8:"b",
+            9:"c"
+        }
+    }
+}
+
+// console.log(allObj);
+
+
+// console.log(Object.keys(allObj));
+// console.log(Object.values(allObj));
+
+// console.log(Object.keys(obj));
+// console.log(Object.values(obj));
+
+
+
+// Object.freeze(obj)
+// obj.name = "New"
+
+console.log(obj);
